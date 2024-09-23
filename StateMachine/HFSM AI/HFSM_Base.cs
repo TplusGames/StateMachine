8492805199d14f38f06196eb65af5f102f3578c2
TplusGames <134178294+TplusGames@users.noreply.github.com>
2024-09-23 12:13:08 -0500

    Changes HSFM to run off of designated tickspeed instead of update loop

--- StateMachine/HFSM AI/HFSM_Base.cs
+++ StateMachine/HFSM AI/HFSM_Base.cs
@@ -5,8 +5,14 @@ namespace TPlus.StateMachine
     public abstract class HFSM_Base : MonoBehaviour
     {
         protected StateMachine_Base _currentStateMachine;
+        protected float _tickSpeed = 0.1f;
 
-        protected virtual void Update()
+        protected virtual void Start()
+        {
+            InvokeRepeating(nameof(SMUpdate), _tickSpeed, _tickSpeed);
+        }
+        
+        protected virtual void SMUpdate()
         {
             _currentStateMachine?.StateUpdate();
         }
